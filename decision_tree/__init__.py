fc89d1c02e1a3138ec2552a2dc3b0128f94b4e70
Guilherme <guilhermekoerich@gmail.com>
2020-05-09 11:41:49 -0300

    Add RandomForest and RandomForestCredit

--- decision_tree/__init__.py
+++ decision_tree/__init__.py
@@ -1,3 +1,8 @@
 from .tree import Tree, TreeCriterion
+from .tree_credit_data import TreeCredit
+from .tree_census import TreeCensus
+from .random_forest import RandomForest
+from .random_forest_credit import RandomForestCredit
 
-__all__ = ['TreeCriterion', 'Tree']
+__all__ = ['TreeCriterion', 'Tree', 'RandomForest', 'TreeCensus', 'TreeCredit', 'RandomForestCredit']
+
---
+++ decision_tree/random_forest_credit.py
@@ -0,0 +1,19 @@
+from decision_tree import RandomForest, TreeCriterion
+from bases import CreditBase
+
+
+class RandomForestCredit(RandomForest):
+    __slots__ = []
+
+    __base = CreditBase()
+
+    def __init__(self, criterion=TreeCriterion.ENTROPY.value, trees=10):
+        super().__init__(self.__base, criterion, trees=trees)
+
+    def test_base(self):
+        self._test_base()
+        print(f'Precisão {self.precision}%')
+
+
+if __name__ == '__main__':
+    RandomForestCredit(trees=10).test_base()
